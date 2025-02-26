4cb3a11bb50a431b5e5b421a8830b57695e6b646
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-02-26 11:35:35 +0530

    Added Collecting to Set

--- src/CollectorsDemo.java
+++ src/CollectorsDemo.java
@@ -9,6 +9,9 @@ public class CollectorsDemo {
         List<String> newNames = names.stream().filter(s -> s.startsWith("R")).collect(Collectors.toList());
         System.out.println(newNames);
 
-        
+        // 2) Collecting to Set
+        List<Integer> nums = Arrays.asList(1,2,4,5,3,2,2,3,1,4,6,7,3,5,6,8);
+        Set<Integer> newNums = nums.stream().collect(Collectors.toSet());
+        System.out.println(newNums);
     }
 }
