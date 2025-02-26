c21cf8a9b126acf8b1daa110010573795b2aa760
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-02-26 15:03:30 +0530

    Added Question 3

--- src/Practice.java
+++ src/Practice.java
@@ -13,5 +13,10 @@ public class Practice {
         String sentence = new String("Hello Mr. Ekansh, Say Hello to this World. Dont you think this world is worth saying Hello?");
         System.out.println(Arrays.stream(sentence.split(" ")).collect(Collectors.groupingBy(x -> x.replace(",","").replace("?", "").replace(".", "") , Collectors.counting())));
 
+        // Question 3: Partitioning even and odd numbers
+        List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11);
+        System.out.println(nums.stream().collect(Collectors.partitioningBy(x -> x%2==0)));
+
+        
     }
 }
